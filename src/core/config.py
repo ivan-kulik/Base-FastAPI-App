1010bb5cb6d8c11d92d99eae6feeb6ed9d351553
ivan kulik <ivankulik1978@gmail.com>
2024-11-25 13:19:23 +0300

    create database config

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,8 @@
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    PostgresDsn,
+)
 
 
 class RunConfig(BaseModel):
@@ -11,9 +14,18 @@ class APIPrefixConfig(BaseModel):
     prefix: str = '/api'
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: APIPrefixConfig = APIPrefixConfig()
+    db: DatabaseConfig
 
 
 settings = Settings()
